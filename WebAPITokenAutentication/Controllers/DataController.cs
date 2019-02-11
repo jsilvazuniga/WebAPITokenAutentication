3fcf4fc118705f962633e431f683b9cc7baca5c9
jrsz <jsilvazuniga@gmail.com>
2019-02-11 23:31:07 +0100

    first commit

---
+++ WebAPITokenAutentication/Controllers/DataController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace WebAPITokenAutentication.Controllers
+{
+    public class DataController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/data/forall")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Now server time is " + DateTime.Now.ToLongDateString());
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/data/authenticate")]
+        public IHttpActionResult GetForAutentication()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+
+            return Ok("Hello " + identity.Name);
+        }
+
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("api/data/autorize")]
+        public IHttpActionResult GetForAdmin()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+
+            var roles = identity.Claims.Where(c => c.Type == ClaimTypes.Role)
+                                        .Select(c => c.Value);
+
+            return Ok("Hello " + identity.Name + " Roles: " + string.Join(",", roles.ToList()));
+        }
+    }
+}
---
+++ WebAPITokenAutentication/MyAuthorizationServerProvider.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebAPITokenAutentication
+{
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identify = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            if (context.UserName == "admin" && context.Password == "admin")
+            {
+                identify.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identify.AddClaim(new Claim("username", "admin"));
+                identify.AddClaim(new Claim(ClaimTypes.Name, "JORGE SILVA ZUNIGA"));
+                context.Validated(identify);
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identify.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identify.AddClaim(new Claim("username", "user"));
+                identify.AddClaim(new Claim(ClaimTypes.Name, "CAMILLE GADEL"));
+                context.Validated(identify);
+            }
+            else
+            {
+                context.SetError("Invalid_grant", "username and password is incorrect");
+                return;
+            }
+        }
+
+
+        //public override Task ValidateClientAutorization(OAuthValidateClientAuthenticationContext context)
+        //{
+        //    context.Validated();
+        //}
+
+
+
+
+        //public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        //{
+        //    var identify = new ClaimsIdentity(context.Options.AuthenticationType);
+
+        //    if(context.UserName == "admin" && context.Password == "admin")
+        //    {
+        //        identify.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+        //        identify.AddClaim(new Claim("username", "admin"));
+        //        identify.AddClaim(new Claim(ClaimTypes.Name, "JORGE SILVA ZUNIGA"));
+        //        context.Validated(identify);
+        //    }else if (context.UserName == "user" && context.Password == "user")
+        //    {
+        //        identify.AddClaim(new Claim(ClaimTypes.Role, "user"));
+        //        identify.AddClaim(new Claim("username", "user"));
+        //        identify.AddClaim(new Claim(ClaimTypes.Name, "CAMILLE GADEL"));
+        //        context.Validated(identify);
+        //    }
+        //    else
+        //    {
+        //        context.SetError("Invalid_grant", "username and password is incorrect");
+        //        return;
+        //    }
+        //}
+
+
+    }
+}
